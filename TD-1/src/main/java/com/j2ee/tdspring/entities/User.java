cbb4ade49e10d73757e9d4ce175aa1f1784f3e3b
Antoine Mousset <antoine.mousset@ynov.com>
2021-11-23 11:27:48 +0100

    feat: tp 2

--- TD-1/src/main/java/com/j2ee/tdspring/entities/User.java
+++ TD-1/src/main/java/com/j2ee/tdspring/entities/User.java
@@ -4,22 +4,26 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.Valid;
 
 @Entity
 @Table(name="user")
 public class User {
 	
 	 	@Id
+	 	@Valid
 	    @Column(name="Username")
 	    private String username;
 
+	 	@Valid
 	    @Column(name="firstname", length=30, nullable=false)
 	    private String firstname;
 
-	    
+	 	@Valid
 	    @Column(name="lastname", length=30, nullable=false)
 	    private String lastname;
 
+	 	@Valid
 	    @Column(name="telephone")
 	    private int telephone;
 
